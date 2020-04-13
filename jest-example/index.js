dab6d5f7cee987e027e4234ae6f81f94e783cc7e
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-13 19:13:40 -0300

    moore tests

--- jest-example/index.js
+++ jest-example/index.js
@@ -2,13 +2,59 @@ const suma = (a, b) => a + b;
 
 const resta = (a, b) => a - b;
 
-const sumaArray = arr => arr.reduce((a, b) => suma(a, b), 0);
+const sumaArray = arr => {
+  let flag = false;
+  arr.forEach(value => {
+    if (typeof value !== 'number') {
+      flag = true;
+    }
+  });
 
-const restaArray = arr => arr.reduce((a, b) => resta(a, b), 0);
+  if (flag) {
+    return;
+  }
+
+  const result = arr.reduce((a, b) => suma(a, b), 0);
+
+  return result;
+};
+
+const restaArray = arr => {
+  let flag = false;
+  arr.forEach(value => {
+    if (typeof value !== 'number') {
+      flag = true;
+    }
+  });
+
+  if (flag) {
+    return;
+  }
+
+  const result = arr.reduce((a, b) => resta(a, b), 0);
+
+  return result;
+};
+
+const calculadora = (operation, arr) => {
+  switch (operation) {
+    case 'suma':
+      return sumaArray(arr);
+      break;
+    case 'resta':
+      return restaArray(arr);
+      break;
+
+    default:
+      return 'Operacion Invalida';
+      break;
+  }
+}
 
 module.exports = {
   suma,
   resta,
   sumaArray,
   restaArray,
+  calculadora,
 };
--- jest-example/tests/example.test.js
+++ jest-example/tests/example.test.js
@@ -1,59 +1,65 @@
-const { suma, resta, sumaArray, restaArray } = require('../index');
+const { suma, resta, sumaArray, restaArray, calculadora } = require('../index');
 
-// describe('Bateria de tests', () => {
-//   test('Test 1', () => {
-//     const a = true;
+describe('Bateria de tests', () => {
+  test('Test 1', () => {
+    const a = true;
 
-//     expect(a).toBe(true);
-//   });
+    expect(a).toBe(true);
+  });
 
-//   test('Test 2', () => {
-//     const b = 'keepcoding';
+  test('Test 2', () => {
+    const b = 'keepcoding';
 
-//     expect(b).toBe('keepcoding');
-//   });
-// });
+    expect(b).toBe('keepcoding');
+  });
+});
 
-// describe('Test de la funcion suma', () => {
-//   test('Dos valores positivos', () => {
-//     const result = suma(2, 3);
+describe('Test de la funcion suma', () => {
+  test('Dos valores positivos', () => {
+    const result = suma(2, 3);
 
-//     expect(result).not.toBeUndefined();
-//     expect(result).toBe(5);
-//   });
+    expect(result).not.toBeUndefined();
+    expect(result).toBe(5);
+  });
 
-//   test('Un valor negativo', () => {
-//     const result = suma(-3, 2);
+  test('Un valor negativo', () => {
+    const result = suma(-3, 2);
 
-//     expect(result).toBe(-1);
-//   })
-// });
+    expect(result).toBe(-1);
+  })
+});
 
-// describe('Test de la funcion resta', () => {
-//   test('Restar dos valores positivos', () => {
-//     const result = resta(15, 5);
+describe('Test de la funcion resta', () => {
+  test('Restar dos valores positivos', () => {
+    const result = resta(15, 5);
 
-//     expect(result).toBe(10);
-//   });
+    expect(result).toBe(10);
+  });
 
-//   test('Restar dos valores negativos', () => {
-//     const result = resta(-10, -5);
+  test('Restar dos valores negativos', () => {
+    const result = resta(-10, -5);
 
-//     expect(result).toBe(-5);
-//   });
+    expect(result).toBe(-5);
+  });
 
-//   test('Restar un valor positivo otro negativo', () => {
-//     const result = resta(-10, 5);
+  test('Restar un valor positivo otro negativo', () => {
+    const result = resta(-10, 5);
 
-//     expect(result).toBe(-15);
-//   });
-// });
+    expect(result).toBe(-15);
+  });
+});
 
 describe('Test de sumaArray', () => {
   test('Suma array', () => {
     const result = sumaArray([1, 2, 3, 4]);
 
     expect(result).toBe(10);
+  });
+
+  test('Cadenas', () => {
+    const result = sumaArray(['Hola', ' ', 'Mundo']);
+
+    expect(result).toBeUndefined();
   });
 });
 
@@ -63,4 +69,24 @@ describe('Test de restaArray', () => {
 
     expect(result).toBe(-10);
   });
+
+  test('Valores no permitidos', () => {
+    const result = restaArray([4, 'null', true]);
+
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('Test de Calculadora', () => {
+  test('Calculadora suma', () => {
+    const result = calculadora('suma', [1, 2, 3, 4]);
+
+    expect(result).toBe(10);
+  });
+  test('Calculadora default', () => {
+    const result = calculadora('multiplicar', [1, 2, 3, 4]);
+
+    expect(result).toBe('Operacion Invalida');
+  });
+
 });
