df13b8caa3887c4231c0994c3aea9c3c5a794209
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-13 18:02:00 -0300

    first test suit created

---
+++ jest-example/tests/example.test.js
@@ -0,0 +1,13 @@
+describe('Bateria de tests', () => {
+  test('Test 1', () => {
+    const a = true;
+
+    expect(a).toBe(true);
+  });
+
+  test('Test 2', () => {
+    const b = 'keepcoding';
+
+    expect(b).toBe('keepcoding');
+  });
+});
