47169e0fafe41910167da20d379d56f8fcb47605
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-13 16:58:15 -0300

    selenium example

--- selenium-example/index.js
+++ selenium-example/index.js
@@ -1,2 +1,25 @@
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+
+const { By, Key, until, Builder, Capabilities } = webdriver;
+const chromeDriver = './chromedriver';
+
+(async function example() {
+
+  chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver).build());
+  const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+
+  try {
+    //Definición de nuestros test
+    await driver.get('https://keepcoding.io/es/');
+
+    await driver.findElement(By.linkText('¿Quiénes somos?')).click();
+
+    await driver.findElement(By.className('standard-logo')).click();
+
+    await driver.quit();
+  } catch (error) {
+    console.log(error);
+    await driver.quit();
+  }
+})();
