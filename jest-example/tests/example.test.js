85c4c6a4865a5c2c5557ca85bfe13e8e3e4a5538
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-13 18:25:50 -0300

    test resta

--- jest-example/tests/example.test.js
+++ jest-example/tests/example.test.js
@@ -1,3 +1,5 @@
+const { suma, resta } = require('../index');
+
 describe('Bateria de tests', () => {
   test('Test 1', () => {
     const a = true;
@@ -9,5 +11,40 @@ describe('Bateria de tests', () => {
     const b = 'keepcoding';
 
     expect(b).toBe('keepcoding');
+  });
+});
+
+describe('Test de la funcion suma', () => {
+  test('Dos valores positivos', () => {
+    const result = suma(2, 3);
+
+    expect(result).not.toBeUndefined();
+    expect(result).toBe(5);
+  });
+
+  test('Un valor negativo', () => {
+    const result = suma(-3, 2);
+
+    expect(result).toBe(-1);
+  })
+});
+
+describe('Test de la funcion resta', () => {
+  test('Restar dos valores positivos', () => {
+    const result = resta(15, 5);
+
+    expect(result).toBe(10);
+  });
+
+  test('Restar dos valores negativos', () => {
+    const result = resta(-10, -5);
+
+    expect(result).toBe(-5);
+  });
+
+  test('Restar un valor positivo otro negativo', () => {
+    const result = resta(-10, 5);
+
+    expect(result).toBe(-15);
   });
 });
