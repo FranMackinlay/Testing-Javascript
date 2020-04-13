86a705de1bae1777f59c1f2dcc4fcfb41594cb51
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-13 18:45:24 -0300

    more tests

--- jest-example/index.js
+++ jest-example/index.js
@@ -2,7 +2,13 @@ const suma = (a, b) => a + b;
 
 const resta = (a, b) => a - b;
 
+const sumaArray = arr => arr.reduce((a, b) => suma(a, b), 0);
+
+const restaArray = arr => arr.reduce((a, b) => resta(a, b), 0);
+
 module.exports = {
   suma,
   resta,
+  sumaArray,
+  restaArray,
 };
--- jest-example/tests/example.test.js
+++ jest-example/tests/example.test.js
@@ -1,50 +1,66 @@
-const { suma, resta } = require('../index');
+const { suma, resta, sumaArray, restaArray } = require('../index');
 
-describe('Bateria de tests', () => {
-  test('Test 1', () => {
-    const a = true;
+// describe('Bateria de tests', () => {
+//   test('Test 1', () => {
+//     const a = true;
 
-    expect(a).toBe(true);
-  });
+//     expect(a).toBe(true);
+//   });
 
-  test('Test 2', () => {
-    const b = 'keepcoding';
+//   test('Test 2', () => {
+//     const b = 'keepcoding';
 
-    expect(b).toBe('keepcoding');
-  });
-});
+//     expect(b).toBe('keepcoding');
+//   });
+// });
 
-describe('Test de la funcion suma', () => {
-  test('Dos valores positivos', () => {
-    const result = suma(2, 3);
+// describe('Test de la funcion suma', () => {
+//   test('Dos valores positivos', () => {
+//     const result = suma(2, 3);
 
-    expect(result).not.toBeUndefined();
-    expect(result).toBe(5);
-  });
+//     expect(result).not.toBeUndefined();
+//     expect(result).toBe(5);
+//   });
 
-  test('Un valor negativo', () => {
-    const result = suma(-3, 2);
+//   test('Un valor negativo', () => {
+//     const result = suma(-3, 2);
 
-    expect(result).toBe(-1);
-  })
-});
+//     expect(result).toBe(-1);
+//   })
+// });
 
-describe('Test de la funcion resta', () => {
-  test('Restar dos valores positivos', () => {
-    const result = resta(15, 5);
+// describe('Test de la funcion resta', () => {
+//   test('Restar dos valores positivos', () => {
+//     const result = resta(15, 5);
+
+//     expect(result).toBe(10);
+//   });
+
+//   test('Restar dos valores negativos', () => {
+//     const result = resta(-10, -5);
+
+//     expect(result).toBe(-5);
+//   });
+
+//   test('Restar un valor positivo otro negativo', () => {
+//     const result = resta(-10, 5);
+
+//     expect(result).toBe(-15);
+//   });
+// });
+
+describe('Test de sumaArray', () => {
+  test('Suma array', () => {
+    const result = sumaArray([1, 2, 3, 4]);
 
     expect(result).toBe(10);
   });
+});
 
-  test('Restar dos valores negativos', () => {
-    const result = resta(-10, -5);
+describe('Test de restaArray', () => {
+  test('Resta array', () => {
+    const result = restaArray([1, 2, 3, 4]);
 
-    expect(result).toBe(-5);
-  });
-
-  test('Restar un valor positivo otro negativo', () => {
-    const result = resta(-10, 5);
-
-    expect(result).toBe(-15);
+    expect(result).toBe(-10);
   });
 });
